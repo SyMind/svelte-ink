ffca3c419517782bb91242dac6b98f53baef67b4
SyMind <dacongsama@live.com>
2023-01-20 21:05:21 +0800

    init create svelte ink app

---
+++ packages/create-svelte-ink-app/template/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { onDestroy } from 'svelte'
+
+    let counter = 0
+
+    const timer = setInterval(() => {
+        counter += 1
+    }, 100)
+
+    onDestroy(() => clearInterval(timer))
+</script>
+
+<ink-text color="green">{counter} tests passed</ink-text>
---
+++ packages/create-svelte-ink-app/template/src/cli.js
@@ -0,0 +1,4 @@
+import { render } from 'svelte-ink'
+import App from './App.svelte'
+
+render(App)
