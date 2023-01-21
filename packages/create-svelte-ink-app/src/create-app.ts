c48f623dc0f51fcc2dd1ae5d41a6b03587cf10dc
SyMind <dacongsama@live.com>
2023-01-21 20:46:54 +0800

    ensure folder created

--- packages/create-svelte-ink-app/src/create-app.ts
+++ packages/create-svelte-ink-app/src/create-app.ts
@@ -1,5 +1,9 @@
 import path from 'path'
 import { PackageManager } from './get-pkg-manager'
+import { isWriteable } from './is-writeable'
+import { isFolderEmpty } from './is-folder-empty'
+import { makeDir } from './make-dir'
+
 import { installTemplate } from './template'
 
 interface CreateAppArgs {
@@ -10,7 +14,22 @@ interface CreateAppArgs {
 export async function createApp({ appPath, packageManager }: CreateAppArgs) {
     const root = path.resolve(appPath)
 
+    if (!(await isWriteable(path.dirname(root)))) {
+        console.error(
+            'The application path is not writable, please check folder permissions and try again.'
+        )
+        console.error(
+            'It is likely you do not have write permissions for this folder.'
+        )
+        process.exit(1)
+    }
+
     const appName = path.basename(root)
+
+    await makeDir(root)
+    if (!isFolderEmpty(root, appName)) {
+        process.exit(1)
+    }
 
     await installTemplate({
         appName,
